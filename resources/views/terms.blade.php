296ff45d699a793050daeb7558abccbc04871b5c
Volodymyr Honcharov <ladimirvm87@gmail.com>
2024-08-31 19:28:46 +0300

    blade templates and main layout

---
+++ resources/views/terms.blade.php
@@ -0,0 +1,11 @@
+@extends('layout.layout')
+
+@section('content')
+
+<h1>Terms</h1>
+<div>
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam, voluptates est. Dolore delectus, blanditiis fugit
+    voluptas iusto atque nisi assumenda neque sequi quasi qui corporis, minus saepe illo est sapiente!
+</div>
+
+@endsection
