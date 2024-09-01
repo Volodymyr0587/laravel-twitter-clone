ff87f9223b5519fefd4a2a72bfe624f356d3f9b7
Volodymyr Honcharov <ladimirvm87@gmail.com>
2024-09-01 16:23:28 +0300

    AlpineJS for remove flash messages

--- resources/views/shared/success-message.blade.php
+++ resources/views/shared/success-message.blade.php
@@ -1,5 +1,6 @@
 @if (session()->has('success'))
-<div class="alert alert-success alert-dismissible fade show" role="alert">
+<div x-data="{show: true}" x-init="setTimeout(() => show = false, 5000)" x-show="show"
+class="alert alert-success alert-dismissible fade show" role="alert">
     {{ session()->get('success') }}
     <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
 </div>
