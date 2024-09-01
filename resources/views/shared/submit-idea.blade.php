68a736aab71a82ce749f805167a52d29db0c2e22
Volodymyr Honcharov <ladimirvm87@gmail.com>
2024-09-01 16:34:18 +0300

    Validation: show validation errors

--- resources/views/shared/submit-idea.blade.php
+++ resources/views/shared/submit-idea.blade.php
@@ -3,7 +3,10 @@
     <form action="{{ route('idea.create') }}" method="post">
         @csrf
         <div class="mb-3">
-            <textarea class="form-control" name="content" id="content" rows="3"></textarea>
+            <textarea class="form-control" name="content" id="content" rows="3">{{ old('content') }}</textarea>
+            @error('content')
+                <spap class="d-block fs-6 text-danger mt-2">{{ $message }}</spap>
+            @enderror
         </div>
         <div class="">
             <button type="submit" class="btn btn-dark"> Share </button>
