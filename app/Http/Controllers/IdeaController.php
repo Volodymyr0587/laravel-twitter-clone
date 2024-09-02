53bea51d59c898f9b64ffd73deacf7e73ddd6a8c
Volodymyr Honcharov <ladimirvm87@gmail.com>
2024-09-02 15:17:01 +0300

    UPDATE (CRUD)

--- app/Http/Controllers/IdeaController.php
+++ app/Http/Controllers/IdeaController.php
@@ -12,7 +12,6 @@ class IdeaController extends Controller
         return view('ideas.show', compact('idea'));
     }
 
-
     public function store(Request $request)
     {
         $data = $request->validate([
@@ -22,6 +21,24 @@ class IdeaController extends Controller
         Idea::create($data);
 
         return to_route('dashboard')->with('success', 'Idea was created successfully');
+    }
+
+    public function edit(Idea $idea)
+    {
+        $editing = true;
+
+        return view('ideas.show', compact('idea', 'editing'));
+    }
+
+    public function update(Request $request, Idea $idea)
+    {
+        $data = $request->validate([
+            'content' => 'required|string|min:2|max:1000',
+        ]);
+
+        $idea->update($data);
+
+        return to_route('ideas.show', $idea)->with('success', 'Idea was updated successfully');
     }
 
     public function destroy(Idea $idea)
--- resources/views/shared/idea-card.blade.php
+++ resources/views/shared/idea-card.blade.php
@@ -13,6 +13,7 @@
                 <form action="{{ route('ideas.destroy', $idea) }}" method="POST">
                     @csrf
                     @method('DELETE')
+                    <a class="mx-2" href="{{ route('ideas.edit', $idea) }}">Edit</a>
                     <a href="{{ route('ideas.show', $idea) }}">View</a>
                     <button class="ms-1 btn btn-danger btn-sm">X</button>
                 </form>
@@ -20,9 +21,25 @@
         </div>
     </div>
     <div class="card-body">
+        @if ($editing ?? false)
+        <form action="{{ route('ideas.update', $idea) }}" method="post">
+            @csrf
+            @method('PUT')
+            <div class="mb-3">
+                <textarea class="form-control" name="content" id="content" rows="3">{{ $idea->content }}</textarea>
+                @error('content')
+                    <spap class="d-block fs-6 text-danger mt-2">{{ $message }}</spap>
+                @enderror
+            </div>
+            <div class="">
+                <button type="submit" class="btn btn-sm btn-dark mb-2"> Update </button>
+            </div>
+        </form>
+        @else
         <p class="fs-6 fw-light text-muted">
             {{ $idea->content }}
         </p>
+        @endif
         <div class="d-flex justify-content-between">
             <div>
                 <a href="#" class="fw-light nav-link fs-6"> <span class="fas fa-heart me-1">
