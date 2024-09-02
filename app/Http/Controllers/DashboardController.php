85f268863f1eda0dabc6cdae8d540a31e6cb1e96
Volodymyr Honcharov <ladimirvm87@gmail.com>
2024-09-02 17:03:22 +0300

    Search functionality: Pass the search term to the pagination links

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -9,17 +9,21 @@ class DashboardController extends Controller
 {
     public function index()
     {
-        $ideas = Idea::latest();
+        $query = Idea::latest();
 
         //% Check if there is a search
         if (request()->has('search')) {
             request()->validate([
                 'search' => 'required|string|min:1|max:255'
             ]);
-            $ideas = $ideas->where('content', 'LIKE', '%' . request()->get('search', '') . '%');
+            $search = request()->get('search', '');
+            $query = $query->where('content', 'LIKE', "%{$search}%");
         }
 
+        $ideas = $query->paginate(5);
+        // Pass the search term to the pagination links
+        $ideas->appends(['search' => request()->get('search', '')]);
 
-        return view('dashboard', ['ideas' => $ideas->paginate(5)]);
+        return view('dashboard', compact('ideas'));
     }
 }
