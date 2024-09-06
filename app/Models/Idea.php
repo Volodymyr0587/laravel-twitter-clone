44eecedaf7d9d17542028cf30d69d034c8420742
Volodymyr Honcharov <ladimirvm87@gmail.com>
2024-09-06 20:51:38 +0300

    Eager Loading;  property

--- app/Models/Idea.php
+++ app/Models/Idea.php
@@ -11,6 +11,8 @@ class Idea extends Model
 {
     use HasFactory;
 
+    protected $with = ['user:id,name,image', 'comments.user:id,name,image'];
+
     protected $fillable = [
         'user_id',
         'content',
