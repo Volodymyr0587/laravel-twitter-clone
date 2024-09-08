a8d0e1e284aef59d84029aa6fb337059dbd17894
Volodymyr Honcharov <ladimirvm87@gmail.com>
2024-09-08 15:11:27 +0300

    Guest middleware for registration and login pages

--- routes/auth.php
+++ routes/auth.php
@@ -4,15 +4,17 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 
 
-//% Registraion
-Route::get('/register', [AuthController::class, 'register'])->name('register');
+Route::middleware('guest')->group(function () {
+    //% Registraion
+    Route::get('/register', [AuthController::class, 'register'])->name('register');
 
-Route::post('/register', [AuthController::class, 'store']);
+    Route::post('/register', [AuthController::class, 'store']);
 
-//% Login
-Route::get('/login', [AuthController::class, 'login'])->name('login');
+    //% Login
+    Route::get('/login', [AuthController::class, 'login'])->name('login');
 
-Route::post('/login', [AuthController::class, 'authenticate']);
+    Route::post('/login', [AuthController::class, 'authenticate']);
+});
 
 //% Logout
 Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth')->name('logout');
