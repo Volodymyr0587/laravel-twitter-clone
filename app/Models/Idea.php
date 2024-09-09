87520712f5ce8ad9cb6233e7fa5670dc272ca6a5
Volodymyr Honcharov <ladimirvm87@gmail.com>
2024-09-09 15:48:05 +0300

    WithCount for likes

--- app/Models/Idea.php
+++ app/Models/Idea.php
@@ -14,6 +14,8 @@ class Idea extends Model
 
     protected $with = ['user:id,name,image', 'comments.user:id,name,image'];
 
+    protected $withCount = ['likes'];
+
     protected $fillable = [
         'user_id',
         'content',
