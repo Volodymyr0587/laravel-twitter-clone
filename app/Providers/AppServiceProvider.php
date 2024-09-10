b57583545607dcc456d7c77a4c521f1f5a73267d
Volodymyr Honcharov <ladimirvm87@gmail.com>
2024-09-10 18:56:05 +0300

    Global Blade variable (show topUsers on every views)

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -6,6 +6,7 @@ use App\Models\User;
 use Illuminate\Pagination\Paginator;
 use Illuminate\Support\Facades\Gate;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\View;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -31,5 +32,15 @@ class AppServiceProvider extends ServiceProvider
         Gate::define('admin', function (User $user) : bool {
             return (bool) $user->is_admin;
         });
+
+        //% Share $topUsers (Users with the most ideas) variable between views
+        View::share(
+            'topUsers',
+            User::withCount('ideas')
+                ->orderBy('ideas_count', 'DESC')
+                ->limit(5)
+                ->get()
+        );
+
     }
 }
