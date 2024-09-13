835820ec619d35d58150ac1ad32161cbef469acd
Volodymyr Honcharov <ladimirvm87@gmail.com>
2024-09-13 16:06:57 +0300

    Caching topUsers

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use App\Models\User;
 use Illuminate\Pagination\Paginator;
+use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\Gate;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Facades\View;
@@ -29,17 +30,22 @@ class AppServiceProvider extends ServiceProvider
         Paginator::useBootstrapFive();
 
         //% Role
-        Gate::define('admin', function (User $user) : bool {
+        Gate::define('admin', function (User $user): bool {
             return (bool) $user->is_admin;
         });
 
-        //% Share $topUsers (Users with the most ideas) variable between views
+        //% Cache top users
+        $topUsers = Cache::remember('topUsers', now()->addMinutes(5), function () {
+            return User::withCount('ideas')
+                ->orderBy('ideas_count', 'DESC')
+                ->take(5)
+                ->get();
+        });
+
+        //% Share $topUsers (Users with the most ideas) variable between all views
         View::share(
             'topUsers',
-            User::withCount('ideas')
-                ->orderBy('ideas_count', 'DESC')
-                ->limit(5)
-                ->get()
+            $topUsers,
         );
 
     }
